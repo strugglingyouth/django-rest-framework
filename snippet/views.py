129e16e398bfe4d8224e38b891bebe5e365d0fd3
strugglingyouth <630441839@qq.com>
2016-07-18 16:05:26 +0800

    使用类的通用视图重构代码

--- snippet/views.py
+++ snippet/views.py
@@ -1,20 +1,18 @@
 # coding:utf-8
 
-from rest_framework import mixins
 from rest_framework import generics
 from snippet.models import Snippet
 from snippet.serializers import SnippetSerializer
 
 
-class SnippetList(mixins.ListModelMixin, 
-                  mixins.CreateModelMixin, 
-                  generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     """
         展示所有的 snippet 或者新创建的 snippet
-        mixin类则提供了 .list() 和 .ctreae() 的行为
+        generics.ListCeateAPIView 提供了 get，post，create，list 等方法 
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
@@ -22,23 +20,10 @@ class SnippetList(mixins.ListModelMixin,
         return self.create(request, *args, **kwargs)
 
 
-class SnippetDetail(mixins.RetrieveModelMixin, 
-                    mixins.UpdateModelMixin, 
-                    mixins.DestroyModelMixin, 
-                    generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
         修改或删除一个 snippet
+        generics.RetrieveUpdateDestroyAPIView 提供了 delete,get, patch, put 等方法
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs) 
-
-
